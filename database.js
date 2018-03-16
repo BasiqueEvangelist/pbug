de708d2c84b5d889a16b9372350eb46b0d714c35
Nikolay Kochulin <nikolay@porez.ko-chu.local>
2018-03-16 23:47:19 +0300

    Disable postgres

--- database.js
+++ database.js
@@ -28,30 +28,30 @@ module.exports = function (type, host, user, pass, db) {
             }
         };
     }
-    else if (type === "postgresql") {
-        var conn = new pg.Client({
-            host: host,
-            user: user,
-            password: pass,
-            database: db
-        });
-        conn.connect();
-        return {
-            query: function () {
-                var topargs = arguments;
-                if (arguments.length === 3)
-                    debug("performing query \"%s\" with params %o", arguments[0], arguments[1]);
-                else
-                    debug("performing query \"%s\"", arguments[0]);
-                conn.query(arguments[0], arguments.length === 3 ? arguments[1] : [], function (err) {
-                    if (err) {
-                        debug("query error: %o", err);
-                    }
-                    (topargs.length === 3 ? topargs[2] : topargs[1]).apply(this, arguments);
-                });
-            }
-        };
-    }
+    // else if (type === "postgresql") {
+    //     var conn = new pg.Client({
+    //         host: host,
+    //         user: user,
+    //         password: pass,
+    //         database: db
+    //     });
+    //     conn.connect();
+    //     return {
+    //         query: function () {
+    //             var topargs = arguments;
+    //             if (arguments.length === 3)
+    //                 debug("performing query \"%s\" with params %o", arguments[0], arguments[1]);
+    //             else
+    //                 debug("performing query \"%s\"", arguments[0]);
+    //             conn.query(arguments[0], arguments.length === 3 ? arguments[1] : [], function (err) {
+    //                 if (err) {
+    //                     debug("query error: %o", err);
+    //                 }
+    //                 (topargs.length === 3 ? topargs[2] : topargs[1]).apply(this, arguments);
+    //             });
+    //         }
+    //     };
+    // }
     else {
         throw new Error("Database " + type + " not supported");
     }
