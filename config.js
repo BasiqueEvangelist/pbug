ac99b28eb0afef37864beeaf366d1c79c11195a0
Nikolay Kochulin <porez0xfeedface@gmail.com>
2018-12-09 17:04:43 +0300

    Add "include" option to config

--- config.js
+++ config.js
@@ -10,8 +10,29 @@ module.exports = function () {
             "db": ""
         },
         "logo": "static/PB.svg",
-        "port": 8080
+        "port": 8080,
+        "extends": "default",
+        "include": []
     };
-    var userconf = JSON.parse(fs.readFileSync("./config.json"));
+    function getuserconf(path, prevs) {
+        if (prevs.includes(path))
+            return {};
+        var userconf = JSON.parse(fs.readFileSync(path));
+        if (typeof userconf.extends !== "undefined")
+            if (userconf.extends !== "default") {
+                var nprevs = prevs;
+                nprevs.push(path);
+                userconf = Object.assign(getuserconf(userconf.extends, nprevs), userconf);
+            }
+        if (typeof userconf.include !== "undefined") {
+            userconf.include.forEach(e => {
+                var nprevs = prevs;
+                nprevs.push(path);
+                userconf = Object.assign(userconf, getuserconf(e, nprevs));
+            });
+        }
+        return userconf;
+    }
+    var userconf = getuserconf("./config.json", []);
     return Object.assign(defaultconf, userconf);
 };
