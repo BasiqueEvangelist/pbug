a86aa337a3d7887f1a6b003aa8104b3d28f185b0
Nikolay Kochulin <porez0xfeedface@gmail.com>
2018-12-14 21:16:14 +0300

    Change catchFiles to complete everything

--- common.js
+++ common.js
@@ -74,9 +74,15 @@ exports.requiresPermission = function (perm, env, defaultv) {
 exports.catchFiles = function () {
     return [exports.requiresLogin, connect_busboy({}), async function (req, res, next) {
         if (req.method === "POST" && req.get("content-type").startsWith("multipart/form-data")) {
-            req.files = [];
+            req.files = {};
             req.fields = {};
-            req.busboy.on('file', async function (fieldname, file, filename, encoding, mimetype) {
+            var proms = [];
+            function addprom(p) {
+                return function () {
+                    proms.push(p.apply(this, arguments));
+                };
+            }
+            req.busboy.on('file', addprom(async function (fieldname, file, filename, encoding, mimetype) {
                 var fileid = require('crypto').randomBytes(48).toString('base64').replace(/\//g, '_').replace(/\+/g, '-');
                 file.pipe(fs.createWriteStream("files/" + fileid));
                 await connection("files")
@@ -85,12 +91,14 @@ exports.catchFiles = function () {
                         "authorid": req.user.id,
                         "uid": fileid
                     });
-                req.files.push(fileid);
-            });
-            req.busboy.on('field', function (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
+                req.files[fieldname] = fileid;
+            }));
+            req.busboy.on('field', addprom(async function (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
                 req.fields[fieldname] = val;
-            });
-            req.busboy.on('finish', function () {
+            }));
+            req.busboy.on('finish', async function () {
+                console.log(proms);
+                await Promise.all(proms);
                 next();
             });
             req.pipe(req.busboy);
